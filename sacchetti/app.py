36e9ac5b0c247f78e3f2df2439a31df672458bdb
riccardoSacchetti04 <riccardo.sacchetti@itsrizzoli.it>
2025-06-09 17:00:16 +0200

    Update app.py

--- sacchetti/app.py
+++ sacchetti/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request, jsonify
-
+import joblib
 app = Flask(__name__)
+model = joblib.load('sacchetti/bestHeartPred.joblib')
 
 @app.route('/infer', methods=['POST'])
 def hello():
     data = request.get_json()
     name = data.get('name', 'Stranger')
-    return jsonify({"message": "Hello {}!".format(name)})
+    try:
+        data = request.get_json()
+        input_df = pd.DataFrame([data]) if isinstance(data, dict) else pd.DataFrame(data)
+        prediction = model.predict(input_df)
+        return jsonify({"prediction": prediction.tolist()})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
